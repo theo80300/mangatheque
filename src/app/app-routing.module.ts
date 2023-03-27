fb5f4d3c76edb2cdf08556dd1072a95f0b43eefa
theo80300 <theo80300@gmail.com>
2023-03-27 12:08:40 +0200

    add gradie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
-import { MenuGaucheComponent } from './components/menu-gauche/menu-gauche.component';
-import { AccueilComponent } from './components/accueil/accueil.component';
-import { ListeNavbarComponent } from './components/liste-navbar/liste-navbar.component';
-import { Recherche}
+
 
 const routes: Routes = [  { path: '', component: AccueilComponent },
 { path: 'liste', component: ListeNavbarComponent },
